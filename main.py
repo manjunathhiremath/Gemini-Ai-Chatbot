1141c1d3021ad9d0fb1ea7a2b3e11c655c52ab4b
alokpandey098 <kalok0575@gmail.com>
2024-09-23 11:45:51 +0530

    Web app check

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import os 
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(working_dir)
+
+# Setting up the page configuration
+
+st.set_page_config(
+    page_title="AI Chatbot",
+    page_icon="🧠",
+    layout="centered")
+
+with st.sidebar:
+    selected =option_menu(menu_title= "  Gemini AI Chatbot",
+                          options=["Chatbot","Image Captioning","Embedded Text","Ask Me Anything"],
+                          menu_icon="robot",icons=['chat-dots-fill','image-fill','textarea-t',
+                                                   'patch-question-fill'],
+
+                        default_index=0
+                          
+                          
+                          
+                          )

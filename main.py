f893002cfc38d11c534bcd85e698819eecc8ab53
alokpandey098 <kalok0575@gmail.com>
2024-09-23 16:05:43 +0530

    Image captioning Done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os 
-
+from PIL import Image
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-from utills import load_gemini_pro_model
+from utills import (load_gemini_pro_model ,gemini_pro_vision_response)
+
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,9 +19,11 @@ st.set_page_config(
 
 with st.sidebar:
     selected =option_menu(menu_title= "  Gemini AI Chatbot",
-                          options=["Chatbot","Image Captioning","Embedded Text","Ask Me Anything"],
-                          menu_icon="robot",icons=['chat-dots-fill','image-fill','textarea-t',
-                                                   'patch-question-fill'],
+                        options=["Chatbot","Image Captioning","Embedded Text",
+                                 "Ask Me Anything"],
+                        menu_icon="robot",icons=['chat-dots-fill','image-fill',
+                                                 'textarea-t',
+                                                'patch-question-fill'],
 
                         default_index=0             
                           )
@@ -33,6 +36,7 @@ def translate_role_for_streamlit(user_role):
     else:
         return user_role
     
+# Chatbot Section ...    
 if selected == "Chatbot":
 
     model = load_gemini_pro_model()
@@ -49,7 +53,7 @@ if selected == "Chatbot":
             st.markdown(message.parts[0].text)
 
     # User input
-    user_prompt = st.chat_input("Ask Gemini Pro .....")
+    user_prompt = st.chat_input("Ask Gemini Pro ✨...")
 
     if user_prompt:
         st.chat_message("user").markdown(user_prompt)
@@ -59,3 +63,29 @@ if selected == "Chatbot":
         # Display the gemini response
         with st.chat_message("assistant"):
             st.markdown(gemini_response.text)
+
+# Image Camptioning Section 
+if selected == "Image Captioning":
+
+    # Streamlit page title
+    st.title("🎞️ Automated Captioning")
+    uploaded_image = st.file_uploader("Upload an Image🤳...",type=['jpg', 'png','jpeg'])
+    
+    if st.button("Generate Caption"):
+        image = Image.open(uploaded_image)
+        col1,col2 = st.columns(2)
+
+        with col1 :
+            resize_image = image.resize((800,500))
+            st.image(resize_image)
+
+        default_prompt = "write short caption for this image"
+
+        # Getting response from gemini-1.5-flash model
+        caption =gemini_pro_vision_response(default_prompt, image)
+
+        with col2:
+            st.info(caption)
+
+
+
--- utills.py
+++ utills.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+# from PIL import Image
 import google.generativeai as genai
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +15,22 @@ Google_Api_key = config_data["GOOGLE_API_KEY"]
 # Configuring google generativeai with api kay
 genai.configure(api_key=Google_Api_key)
 
+# Function For Chatbot
 def load_gemini_pro_model():
-    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    gemini_pro_model = genai.GenerativeModel("gemini-1.5-flash")
     return gemini_pro_model
+
+# Function For image captioning ..
+def gemini_pro_vision_response(prompt,image):
+    gemini_pro_vision_model = genai.GenerativeModel("gemini-1.5-flash")
+    response = gemini_pro_vision_model.generate_content([prompt,image])
+    result= response.text
+    return result
+
+
+# testing model 
+# image = Image.open("alok.jpg")
+# prompt = "Write short caption for this image"
+# output = gemini_pro_vision_response(prompt, image)
+
+# print(output)
